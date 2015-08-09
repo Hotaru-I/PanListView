c5550ab2948e4842dd941daa5b2d87520d2f1422
Hotaru-I <hotaru.i6233@gmail.com>
2015-08-09 21:35:45 +0800

    version 1.0

---
+++ PanListView/PanListView.h
@@ -0,0 +1,13 @@
+//
+//  PanListView.h
+//  PanListView
+//
+//  Created by Hotaru on 2015/8/9.
+//  Copyright (c) 2015年 Hotaru.I. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanListView : UIView
+
+@end
---
+++ PanListView/PanListView.m
@@ -0,0 +1,21 @@
+//
+//  PanListView.m
+//  PanListView
+//
+//  Created by Hotaru on 2015/8/9.
+//  Copyright (c) 2015年 Hotaru.I. All rights reserved.
+//
+
+#import "PanListView.h"
+
+@implementation PanListView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
